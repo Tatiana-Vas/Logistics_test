0acff0e29cc950c150b75385d7685637b8e4eeca
tatiana <tatiana.vasilyeva7@gmail.com>
2024-06-27 13:18:38 +0200

    add cost per day

--- main.py
+++ main.py
@@ -48,13 +48,13 @@ print("\nДанные по месячной выручке:")
 print(monthly_revenue)
 
 # Converting the MONTH column data type to a string
-monthly_revenue['month'] = monthly_revenue['month'].astype(str)
+monthly_revenue['month'] = monthly_revenue['month'].dt.strftime('%B %Y')
 
 # Plotting a graph for revenue by month
 plt.figure(figsize=(10, 6))
 sns.lineplot(data=monthly_revenue, x='month', y='cost', marker='o')
 plt.title('Изменение общей выручки по месяцам')
-plt.xlabel('Месяц')
+plt.xlabel('Месяц', loc='right')
 plt.ylabel('Общая выручка')
 plt.grid(True)
 plt.show()
@@ -64,22 +64,39 @@ merged_data = pd.merge(rentals, wagons, on='wagon_id')
 
 # Revenue by wagon type
 revenue_by_wagon_type = merged_data.groupby('wagon_type')['cost'].sum().reset_index()
+# Sorting revenue by wagon type by cost
+revenue_by_wagon_type = revenue_by_wagon_type.sort_values(by='cost', ascending=False).reset_index(drop=True)
 
 # Number of rentals by wagon type
 rental_count_by_wagon_type = merged_data.groupby('wagon_type')['rental_id'].count().reset_index()
 rental_count_by_wagon_type.rename(columns={'rental_id': 'rental_count'}, inplace=True)
+# Sorting rental count by wagon type by rental_count
+rental_count_by_wagon_type = rental_count_by_wagon_type.sort_values(by='rental_count', ascending=False).\
+    reset_index(drop=True)
+
+# Calculate rental duration in days
+merged_data['rental_duration'] = (merged_data['end_date'] - merged_data['start_date']).dt.days
+# Calculate cost per day
+merged_data['cost_per_day'] = merged_data['cost'] / merged_data['rental_duration']
+# Average cost per day by wagon type
+avg_cost_per_day_by_wagon_type = merged_data.groupby('wagon_type')['cost_per_day'].mean().reset_index()
+# Sorting average cost per day by wagon type by cost_per_day
+avg_cost_per_day_by_wagon_type = avg_cost_per_day_by_wagon_type.sort_values(by='cost_per_day', ascending=False).\
+    reset_index(drop=True)
 
 # Print data
 print("\nДанные по выручке по типам вагонов:")
 print(revenue_by_wagon_type)
 print("\nДанные по количеству аренд по типам вагонов:")
 print(rental_count_by_wagon_type)
+print("\nСредняя стоимость аренды в день по типам вагонов:")
+print(avg_cost_per_day_by_wagon_type)
 
 # Plotting a graph for revenue by wagon type
 plt.figure(figsize=(10, 6))
 sns.barplot(data=revenue_by_wagon_type, x='wagon_type', y='cost')
 plt.title('Общая выручка по типам вагонов')
-plt.xlabel('Тип вагона')
+plt.xlabel('Тип вагона', loc='right')
 plt.ylabel('Общая выручка')
 plt.grid(True)
 plt.show()
@@ -88,7 +105,16 @@ plt.show()
 plt.figure(figsize=(10, 6))
 sns.barplot(data=rental_count_by_wagon_type, x='wagon_type', y='rental_count')
 plt.title('Количество аренд по типам вагонов')
-plt.xlabel('Тип вагона')
+plt.xlabel('Тип вагона', loc='right')
 plt.ylabel('Количество аренд')
+plt.grid(True)
+plt.show()
+
+# Plotting a graph for average cost per day by wagon type
+plt.figure(figsize=(10, 6))
+sns.barplot(data=avg_cost_per_day_by_wagon_type, x='wagon_type', y='cost_per_day')
+plt.title('Средняя стоимость аренды в день по типам вагонов')
+plt.xlabel('Тип вагона', loc='right')
+plt.ylabel('Средняя стоимость аренды в день')
 plt.grid(True)
 plt.show()
